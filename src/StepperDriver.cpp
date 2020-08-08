c0bc7db23e0f69e2050181efcd27bfbe527903a6
Kevin <smeatsan@gmail.com>
2020-08-08 15:13:32 +0200

    Set the license to GPLv3

--- src/StepperDriver.cpp
+++ src/StepperDriver.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020 smeat.
+ *
+ * This file is part of AsyncStepper 
+ * (see https://github.com/Smeat/AsyncStepper).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "StepperDriver.h"
 #include "AsyncStepper.h"
 
@@ -66,6 +84,5 @@ void stepper_init(async_stepper_t* stepper, int steps, int rpm, int dir, int ste
 	digitalWrite(ms3, HIGH);
 	memcpy(stepper->pins, &pins, sizeof(pins));
 	stepper->update = stepper_update;
-	stepper->on = 1;
 	Serial.println("Stepper init done");
 }
